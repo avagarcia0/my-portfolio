f1c109dc76a7c73225e9b4b959f2df249b7c03c3
Ava Garcia <avagarcia@google.com>
2020-06-09 17:50:06 +0000

    Delete all comments with a single call

--- portfolio/src/main/java/com/google/sps/servlets/DeletionServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeletionServlet.java
@@ -17,8 +17,10 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.common.collect.Iterables;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -35,8 +37,7 @@ public class DeletionServlet extends HttpServlet {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
 
-    for (Entity entity : results.asIterable()) {
-      datastore.delete(entity.getKey());
-    }
+    Iterable<Key> keysToDelete = Iterables.transform(results.asIterable(), Entity::getKey);
+    datastore.delete(keysToDelete);
   }
 }
