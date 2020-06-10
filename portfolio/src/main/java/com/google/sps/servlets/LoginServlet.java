b9e739d70ac5f3fbf09bf29898e0949d9b4afeed
Ava Garcia <avagarcia@google.com>
2020-06-10 17:35:12 +0000

    Add description to LoginServlet

--- portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -23,6 +23,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/** Servlet that returns the login URL and status for the user. */
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
   @Override
