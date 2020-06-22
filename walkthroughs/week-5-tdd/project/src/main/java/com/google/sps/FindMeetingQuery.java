599defeb6dab8eaaa2d15d6ec8ff12fc5bd668f4
Ava Garcia <avagarcia@google.com>
2020-06-22 16:01:07 +0000

    Split up query into multiple methods

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -21,69 +21,116 @@ import java.util.List;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    List<TimeRange> possibleTimes = new ArrayList<>();
-    List<TimeRange> conflictingTimes = new ArrayList<>();
-    Collection<String> attendees = request.getAttendees();
+    List<TimeRange> conflictingTimes = findConflictingTimes(events, request.getAttendees());
+    List<TimeRange> possibleTimes = findPossibleTimes(conflictingTimes, request.getDuration());
 
-    // Find every TimeRange that needs to be avoided
+    return possibleTimes;
+  }
+
+  public List<TimeRange> findConflictingTimes(Collection<Event> events,
+      Collection<String> attendees) {
+    List<TimeRange> conflicts = findConflicts(events, attendees);
+    List<TimeRange> conflictingTimes = mergeOverlappingRanges(conflicts);
+
+    return conflictingTimes;
+  }
+
+  public List<TimeRange> findConflicts(Collection<Event> events,
+      Collection<String> attendees) {
+    List<TimeRange> conflicts = new ArrayList<>();
+
     for (Event event : events) {
-      for (String attendee : event.getAttendees()) {
-        if (attendees.contains(attendee)) {
-          conflictingTimes.add(event.getWhen());
+      for (String eventAttendee : event.getAttendees()) {
+        if (attendees.contains(eventAttendee)) {
+          conflicts.add(event.getWhen());
           break;
         }
       }
     }
 
-    Collections.sort(conflictingTimes, TimeRange.ORDER_BY_START);
+    Collections.sort(conflicts, TimeRange.ORDER_BY_START);
 
-    // Remove duplicates and overlaps
+    return conflicts;
+  }
+
+  public List<TimeRange> mergeOverlappingRanges(List<TimeRange> conflicts) {
+    List<TimeRange> conflictingTimes = new ArrayList<>();
+    TimeRange first = null;
+    TimeRange second = null;
+    TimeRange combined = null;
+    boolean nested = false;
+    boolean useCombinedRange = false;
+
+    for (int i = 0; i < conflicts.size() - 1; i++) {
+      if (useCombinedRange) {
+        first = combined;
+        useCombinedRange = false;
+      } else {
+        first = conflicts.get(i);
+      }
+
+      second = conflicts.get(i + 1);
+      nested = false;
+
+      if (!first.overlaps(second)) {
+        conflictingTimes.add(first);
+      } else if (!first.contains(second)) {
+        combined = TimeRange.fromStartEnd(first.start(), second.end(), false);
+        useCombinedRange = true;
+      } else {
+        conflictingTimes.add(first);
+        nested = true;
+
+        // Skip over adding the second TimeRange
+        i++;
+      }
+    }
+
+    if (conflicts.size() == 0 || nested) {
+      return conflictingTimes;
+    }
+
+    if (conflicts.size() == 1) {
+      conflictingTimes.add(conflicts.get(0));
+    } else if (useCombinedRange) {
+      conflictingTimes.add(combined);
+    } else {
+      conflictingTimes.add(second);
+    }
+
+    return conflictingTimes;
+  }
+
+  public List<TimeRange> findPossibleTimes(List<TimeRange> conflictingTimes, long meetingDuration) {
+    List<TimeRange> possibleTimes = new ArrayList<>();
+
+    if (conflictingTimes.isEmpty()) {
+      addTimeIfLongEnough(TimeRange.WHOLE_DAY, meetingDuration, possibleTimes);
+      return possibleTimes;
+    }
+
+    TimeRange initial = conflictingTimes.get(0);
+    TimeRange valid = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, initial.start(), false);
+    addTimeIfLongEnough(valid, meetingDuration, possibleTimes);
+
     for (int i = 0; i < conflictingTimes.size() - 1; i++) {
       TimeRange first = conflictingTimes.get(i);
       TimeRange second = conflictingTimes.get(i + 1);
 
-      if (first.overlaps(second)) {
-        if (!first.contains(second)) {
-          TimeRange combined = TimeRange.fromStartEnd(first.start(), second.end(), false);
-          conflictingTimes.set(i, combined);
-        }
-
-        conflictingTimes.remove(second);
-        i--;
-      }
+      valid = TimeRange.fromStartEnd(first.end(), second.start(), false);
+      addTimeIfLongEnough(valid, meetingDuration, possibleTimes);
     }
 
-    // Find every TimeRange that doesn't have a conflict
-    if (conflictingTimes.isEmpty()) {
-      possibleTimes.add(TimeRange.WHOLE_DAY);
-    } else {
-      TimeRange initial = conflictingTimes.get(0);
-      TimeRange valid = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, initial.start(), false);
-      possibleTimes.add(valid);
-
-      for (int i = 0; i < conflictingTimes.size() - 1; i++) {
-        TimeRange first = conflictingTimes.get(i);
-        TimeRange second = conflictingTimes.get(i + 1);
-
-        valid = TimeRange.fromStartEnd(first.end(), second.start(), false);
-        possibleTimes.add(valid);
-      }
-
-      TimeRange last = conflictingTimes.get(conflictingTimes.size() - 1);
-      valid = TimeRange.fromStartEnd(last.end(), TimeRange.END_OF_DAY, true);
-      possibleTimes.add(valid);
-    }
-
-    // Remove every TimeRange that is too short
-    long duration = request.getDuration();
-
-    for (int i = 0; i < possibleTimes.size(); i++) {
-      if (possibleTimes.get(i).duration() < duration) {
-        possibleTimes.remove(i);
-        i--;
-      }
-    }
+    TimeRange last = conflictingTimes.get(conflictingTimes.size() - 1);
+    valid = TimeRange.fromStartEnd(last.end(), TimeRange.END_OF_DAY, true);
+    addTimeIfLongEnough(valid, meetingDuration, possibleTimes);
 
     return possibleTimes;
+  }
+
+  public void addTimeIfLongEnough(TimeRange time, long minDuration, List<TimeRange> timeList) {
+    if (time.duration() >= minDuration) {
+      timeList.add(time);
+    }
   }
 }
